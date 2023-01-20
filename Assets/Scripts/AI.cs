c920160739fef88569db65d36ea804689458a6bb
aelianh <95858325+aelianh@users.noreply.github.com>
2023-01-20 11:32:15 +0100

    IA-patrol

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    enum State
+    {
+            Patrolling,
+            Chasing,
+
+    }
+
+    State currentState;
+    NavMeshAgent agent;
+
+    public Transform[] destinationPoints;
+    int destinationIndex = 0;
+    public Transform player;
+    float visionRange;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        currentState = State.Patrolling;
+
+        destinationIndex = Random.Range(0, destinationPoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentState)
+        {
+            case State.Patrolling:
+                Patrol();
+            break;
+            
+            case State.Chasing:
+                Chase();
+            break;
+        }
+        
+
+
+    }
+    
+    void Patrol()
+    {
+        agent.destination = destinationPoints[destinationIndex].position;
+        if(Vector3.Distance(transform.position, destinationPoints[destinationIndex].position) < 1)
+        {
+            destinationIndex =  Random.Range(0, destinationPoints.Length);
+        }
+        
+        if(Vector3.Distance(transform.position, player.position) < visionRange)
+        {
+            currentState = State.Chasing;
+        }
+    }
+
+    void Chase()
+    {
+        agent.destination = player.position;
+    }
+
+    void OnDrawGizmos() 
+    {
+        foreach (Transform point in destinationPoints)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(point.position, 1);
+
+        } 
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, visionRange);   
+    }
+}
